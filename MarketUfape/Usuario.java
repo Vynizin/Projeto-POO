5070085a73a1855636ea42f418fe151ea7895e39
Vynícius <davidvynizin@gmail.com>
2026-07-21 20:47:33 -0300

    Adicionando Atributos, construtor e alguns get e setters

--- MarketUfape/Usuario.java
+++ MarketUfape/Usuario.java
@@ -1,4 +1,42 @@
+import java.time.LocalDate;
 
 public class Usuario {
+	
 
+	private int id;
+	private String nome;
+	private String emailInstitucional;
+	private String senha;
+	private LocalDate dataNascimento;
+	private String curso;
+	private String fotoPerfil;
+	private String biografia;
+
+    public Usuario(int id, String nome, String emailInstitucional, String senha,
+            LocalDate dataNascimento, String curso, String fotoPerfil, String biografia) {
+    	this.id = id;
+    	this.nome = nome;
+    	this.emailInstitucional = emailInstitucional;
+    	this.senha = senha;
+    	this.dataNascimento = dataNascimento;
+    	this.curso = curso;
+    	this.fotoPerfil = fotoPerfil;
+    	this.biografia = biografia;
+    }
+	
+   public String getNome() {
+        return nome;
+    }
+    
+   public void setNome(String nome) {
+       this.nome = nome;
+   }
+
+   public String getEmailInstitucional() {
+       return emailInstitucional;
+   }
+
+   public void setEmailInstitucional(String emailInstitucional) {
+       this.emailInstitucional = emailInstitucional;
+   }
 }
