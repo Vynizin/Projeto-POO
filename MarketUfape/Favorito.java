f5eb9991327d26c6da41ef273fa712e5ed2f389c
Maria Águida <aguidavsilva@gmail.com>
2026-07-27 14:05:26 -0300

    Adicionando métodos a classe Favorito

--- MarketUfape/Favorito.java
+++ MarketUfape/Favorito.java
@@ -1,5 +1,40 @@
 public class Favorito {
-	private int id;
-	private Usuario usuario;
-	private Produto produto;
+
+    private int id;
+    private Usuario usuario;
+    private Produto produto;
+
+    public Favorito() {
+    }
+
+    public Favorito(int id, Usuario usuario, Produto produto) {
+        this.id = id;
+        this.usuario = usuario;
+        this.produto = produto;
+    }
+	
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
 }
